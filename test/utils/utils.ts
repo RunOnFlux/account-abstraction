6edfdc1d9400a007b4d7428e38ea54dfed59d14b
Wiktor Sieprawski <wiktor.siepra@gmail.com>
2024-06-25 16:57:37 +0200

    utils: deep equal

--- test/utils/utils.ts
+++ test/utils/utils.ts
@@ -35,3 +35,28 @@ export const encoder = (types, values) => {
   const encodedParams = abiCoder.encode(types, values)
   return encodedParams.slice(2)
 }
+
+export const deepEqual = (obj1, obj2) => {
+  if (obj1 === obj2) {
+    return true
+  }
+
+  if (typeof obj1 !== 'object' || obj1 === null || typeof obj2 !== 'object' || obj2 === null) {
+    return false
+  }
+
+  let keys1 = Object.keys(obj1)
+  let keys2 = Object.keys(obj2)
+
+  if (keys1.length !== keys2.length) {
+    return false
+  }
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false
+    }
+  }
+
+  return true
+}
